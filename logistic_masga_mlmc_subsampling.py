9fa7eac1cd2dc17b5e4d98c8cbe062657566b7a7
msabvid <marcsv87@gmail.com>
2020-10-26 14:16:02 +0000

    arguments to argparse

--- logistic_masga_mlmc_subsampling.py
+++ logistic_masga_mlmc_subsampling.py
@@ -55,8 +55,8 @@ class LogisticNets(nn.Module):
         y = torch.bmm(x,self.params.unsqueeze(2))
         y = self.activation(y)
         
-        loss = loss_fn(y,target).squeeze(2)
-        loss = loss.mean(1)
+        loss = -loss_fn(y,target).squeeze(2) #!!! I put a minus so that it is the loglikelihood. Important for the signs in the Langevin process
+        loss = loss.sum(1)
 
         loss.backward(torch.ones_like(loss))
         return 0 
@@ -115,10 +115,8 @@ class Bayesian_logistic(MLMC):
         """
         nets.zero_grad()
         nets.forward_backward_pass(self.data_X, self.data_Y, U)
-        
-        #nets.params.data.copy_(nets.params.data + h*(1/self.data_size * self._grad_logprior(nets.params.data) + grad_loglik) + \
-        #        sigma * dW)
-        nets.params.data.copy_(nets.params.data - h/2*(-1/self.data_size * self._grad_logprior(nets.params.data) + nets.params.grad) + \
+        subsample_size = U.shape[1]
+        nets.params.data.copy_(nets.params.data + h/2*(1/self.data_size * self._grad_logprior(nets.params.data) + 1/subsample_size * nets.params.grad) + \
                 sigma * dW)
         if torch.isnan(nets.params.mean()):
             raise ValueError
@@ -155,7 +153,6 @@ class Bayesian_logistic(MLMC):
         sf = self.s0 * self.M ** l
         sc = int(sf/self.M)
         sigma_f = 1/math.sqrt(self.data_size)
-        sigma_c = 1/math.sqrt(sc)
 
         sums_level_l = np.zeros(6) # this will store level l sum  and higher order momentums 
 
@@ -173,7 +170,6 @@ class Bayesian_logistic(MLMC):
             for n in range(int(nf)):
                 dW = math.sqrt(hf) * torch.randn_like(dW)
                 U = np.random.choice(self.data_size, (N2,sf), replace=True)
-                #U = np.array([np.random.choice(self.data_size, sf, replace=False) for i in range(N2)])
                 self._euler_step(X_f, U, sigma_f, hf, dW)
                 
                 if l>0:
@@ -194,6 +190,11 @@ class Bayesian_logistic(MLMC):
 
 
     def get_cost(self, l):
+        """
+        Number of random numbers generated:
+            - Number of random numbers generated for Brownian motion: self.n0
+            - In each step in time discretisation, number of random numbers generated for subsampling: self.n0*self.s0*self.M**l
+        """
         cost = self.n0 * (1+self.s0 * self.M ** l)
         return cost
     
@@ -269,16 +270,21 @@ if __name__ == '__main__':
     #CONFIGURATION
     parser = argparse.ArgumentParser()
     parser.add_argument('--M', type=int, default=2, help='refinement value')
-    parser.add_argument('--N', type=int, default=10000, help='samples for convergence tests')
-    parser.add_argument('--L', type=int, default=6, help='levels for convergence tests')
-    parser.add_argument('--s0', type=int, default=2, help='initial value of data batch size')
+    parser.add_argument('--N', type=int, default=1000, help='samples for convergence tests')
+    parser.add_argument('--L', type=int, default=4, help='levels for convergence tests')
+    parser.add_argument('--s0', type=int, default=256, help='initial value of data batch size')
     parser.add_argument('--N0', type=int, default=2, help='initial number of samples')
     parser.add_argument('--Lmin', type=int, default=0, help='minimum refinement level')
     parser.add_argument('--Lmax', type=int, default=8, help='maximum refinement level')
     parser.add_argument('--device', default='cpu', help='device')
-    parser.add_argument('--dim', type=int, default=2, help='dimension of data')
-    parser.add_argument('--data_size', type=int, default=512, help="data_size")
-    parser.add_argument('--T', type=int, default=5, help='horizon time')
+    parser.add_argument('--dim', type=int, default=2, help='dimension of data if type_data==synthetic')
+    parser.add_argument('--data_size', type=int, default=512, help="data_size if type_data==synthetic")
+    parser.add_argument('--T', type=int, default=10, help='horizon time')
+    parser.add_argument('--n_steps', type=int, default=10000, help='number of steps in time discretisation')
+    parser.add_argument('--seed', type=int, default=1, help='seed')
+    parser.add_argument('--type_data', type=str, default="synthetic", help="type of data")
+    parser.add_argument('--estimate_cost_unbiased', action="store_true", help="estimate cost of unbiased estimator to get an MSE")
+    parser.add_argument('--max_subsample_size', type=int, default=2**13, help="if estimate_cost_unbiased==True, maximum subsample size")
     args = parser.parse_args()
     
     if args.device=='cpu' or (not torch.cuda.is_available()):
@@ -289,13 +295,13 @@ if __name__ == '__main__':
 
     # Target Logistic regression, and synthetic data
     data_X, data_Y = get_dataset(m=args.data_size, d=args.dim,
-            type_regression="logistic", data_dir="./data/")
+        type_regression="logistic", type_data=args.type_data, data_dir="./data/")
 
     data_X = data_X.to(device=device)
     data_Y = data_Y.to(device=device)
     
     # path numerical results
-    path_results = "./numerical_results/mlmc/subsampling/logistic_d{}_m{}".format(args.dim, args.data_size)
+    path_results = "./numerical_results/mlmc_subsampling/logistic/{}_d{}_m{}".format(args.type_data, args.dim, args.data_size)
     if not os.path.exists(path_results):
         os.makedirs(path_results)
     
@@ -305,7 +311,7 @@ if __name__ == '__main__':
             'M':args.M,
             'T':args.T,
             's0':args.s0,
-            'n0':100, # number of steps in time discretisation
+            'n0':args.n_steps, # number of steps in time discretisation
             'data_X':data_X,
             'data_Y':data_Y,
             'device':device,
@@ -314,20 +320,33 @@ if __name__ == '__main__':
     # Bayesian log regressor
     bayesian_logregress = Bayesian_logistic(**MLMC_CONFIG)
     
-    # 1. Convergence tests
+    # 1.a Convergence tests
     bayesian_logregress.estimate_alpha_beta_gamma(args.L, args.N, 
             os.path.join(path_results,"convergence_test_s.txt"))
     bayesian_logregress.save_convergence_test(os.path.join(path_results, "logistic_level_s_data.txt"))
 
-    # 2. get complexities
-    Eps = [0.1, 0.01, 0.001,0.0001]#, 0.0005]
-    Nl_list, mlmc_cost, std_cost = bayesian_logregress.get_complexities(Eps, 
-            os.path.join(path_results, "convergence_test_s.txt"))
 
-    # 3. plot
-    bayesian_logregress.plot(Eps, Nl_list, mlmc_cost, std_cost, 
-            os.path.join(path_results,"logistic_level_s.pdf"))
-    
-    # 4. save
-    bayesian_logregress.save(Eps, Nl_list, mlmc_cost, std_cost, 
-            os.path.join(path_results, "logistic_level_s_data.txt"))
+    # 1.b Estimate variance and and N if unbiased estimator unbiased estimato
+    if args.estimate_cost_unbiased:
+        filename = os.path.join(path_results, "estimate_cost_unbiased.txt")
+        with open(filename, "w") as f:
+            f.write("eps,cost\n")
+        Eps = [0.1,0.01,0.001, 0.0001]
+        for eps in Eps:
+            Vl, Ns, cost_MLMC = bayesian_logregress.estimate_V_N_C_unbiased_estimor(eps,levels=5) 
+            with open(filename, "a") as f:
+                f.write("{:2.4e},{2.4e}\n".format(eps, cost_MLMC))
+    else:
+        # 2. get complexities
+        
+        Eps = [0.1, 0.01, 0.001,0.0001]#, 0.0005]
+        Nl_list, mlmc_cost, std_cost = bayesian_logregress.get_complexities(Eps, 
+                os.path.join(path_results, "convergence_test_s.txt"))
+
+        # 3. plot
+        bayesian_logregress.plot(Eps, Nl_list, mlmc_cost, std_cost, 
+                os.path.join(path_results,"logistic_level_s.pdf"))
+        
+        # 4. save
+        bayesian_logregress.save(Eps, Nl_list, mlmc_cost, std_cost, 
+                os.path.join(path_results, "logistic_level_s_data.txt"))
