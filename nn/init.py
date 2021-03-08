bb514e124443ff17c9e48076956449dcb15882ae
msabvid <marcsv87@gmail.com>
2021-03-08 13:20:22 +0000

    random init

--- nn/init.py
+++ nn/init.py
@@ -15,3 +15,7 @@ def init_mode(params: torch.Tensor, filename: str):
     mode = torch.load(filename, map_location=params.device)
     batch_size = params.shape[0]
     return mode['params'].repeat(batch_size, 1)
+
+
+def normal_(params: torch.Tensor, mean=0.0, std=1.0):
+    return std * (mean + torch.randn_like(params))
